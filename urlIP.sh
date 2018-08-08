10715d51ea5b4ee1daa4e954817abcf4e4d76399
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-08-08 14:04:13 +0300

    Update urlIP.sh

--- urlIP.sh
+++ urlIP.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 file='UrlIP.txt'
 
+grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" $file > IP.txt
+          
+grep -v '^[0-9]' $file > url.txt
+          
 while IFS= read -r var
-do
+do      
 
-if [[ $var =~ ^[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+$ ]]; then
-  echo "success"
+if [[ $var =~ ^[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+$ ]];
+then  
+  echo "$var : success"
 fi
-
+    
 done < "$file"
